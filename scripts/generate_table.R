bed047f4a46e9e1f77c4dbe99c44c7fbe1df6000
mrzdcmps <moritz.dechamps@gmail.com>
2025-07-13 12:08:14 +0200

    start update 2025

--- scripts/generate_table.R
+++ scripts/generate_table.R
@@ -2,14 +2,73 @@ library(tidyverse)
 library(knitr)
 library(kableExtra)
 
+links = c(
+  "studies/lovekind.html",
+  "studies/prayer.html",
+  "studies/monks.html",
+  "studies/monks.html",
+  "studies/monks.html",
+  "studies/monks.html",
+  "studies/meditation.html",
+  "studies/meditation.html",
+  "studies/sound.html",
+  "studies/allgood.html",
+  "studies/bandit.html",
+  "studies/cointoss.html",
+  "studies/incongruence.html",
+  "studies/incongruence.html",
+  "studies/smokers1.html",
+  "studies/smokers1.html",
+  "studies/smokers2.html",
+  "studies/smokers2.html",
+  "studies/smokers3.html",
+  "studies/psysc.html",
+  "studies/psysc.html",
+  "studies/psysc.html",
+  "studies/psysc.html",
+  "studies/psysc.html",
+  "studies/psysc.html",
+  "studies/relax.html",
+  "studies/priming1.html",
+  "studies/priming1.html",
+  "studies/priming2.html",
+  "studies/priming2.html",
+  "studies/priming3.html",
+  "studies/priming3.html",
+  "studies/priming4.html",
+  "studies/priming4.html",
+  "studies/erotic1.html",
+  "studies/erotic2.html",
+  "studies/smokers-priming.html",
+  "studies/smokers-priming.html",
+  "studies/sobjectivity.html",
+  "studies/sobjectivity.html",
+  "studies/sobjectivity.html",
+  "studies/epsi.html",
+  "studies/epsi.html",
+  "studies/robots.html",
+  "studies/games.html",
+  "studies/schroedinger.html",
+  "studies/schroedinger.html",
+  "studies/desire.html",
+  "studies/stories.html",
+  "studies/stories.html",
+  "studies/willpower.html",
+  "studies/willpower.html",
+  "studies/baseline1.html",
+  "studies/baseline1.html",
+  "studies/baseline2.html"
+)
+
 generate_table <- function(experiment_name) {
   
   if(experiment_name == ""){
-    #load("data/study_objects.RData", envir = .GlobalEnv)
-    st <- readRDS("data/overview.rds")
+    #load("data/study_objects.RData", envir = .GlobalEnv) # not needed for overall table
+    st <- readRDS("data/overview2025-07-12.rds")
+    links <- setNames(links, st$Study)
   } else {
-    load("../data/study_objects.RData", envir = .GlobalEnv)
-    st <- readRDS("../data/overview.rds")
+    load("../data/study_objects.RData", envir = .GlobalEnv) # load individual study data
+    st <- readRDS("../data/overview2025-07-12.rds")
   }
   
   ft <- st %>%
@@ -25,7 +84,7 @@ generate_table <- function(experiment_name) {
   
   if(experiment_name == ""){
     ft <- ft %>%
-      mutate(Study = text_spec(Study, link = links))
+      mutate(Study = text_spec(Study, link = links[Study]))
   }
   
   kbl(ft, escape = F,
@@ -45,8 +104,101 @@ generate_table <- function(experiment_name) {
                     "Year",
                     "Lab/Online"),
       align = "lcccccccccccccc") %>%
-    kable_classic("hover", fixed_thead = T)
+    kable_classic("hover") %>%
+    kable_styling(
+      fixed_thead = T,
+      html_font = "Arial",
+      bootstrap_options = c("striped", "hover", "condensed")
+    )
 }
+
+# generate_table_all <- function() {
+#   st <- readRDS("data/overview2025-07-12.rds")
+#   
+#   # Make sure `links` is a named vector like: c("Study A" = "http://...", ...)
+#   # You can load or define `links` here or assume it’s in the global environment
+#   
+#   links <- setNames(links, st$Study)
+#   
+#   ft <- st %>%
+#     mutate(
+#       across(where(is.numeric), ~ round(., 2)),
+#       `Hits (%)` = formattable::color_tile("coral2", "cornflowerblue")(`Hits (%)`),
+#       p = cell_spec(p, bold = ifelse(p < 0.05, TRUE, FALSE)),
+#       Experimental = ifelse(Experimental, "✔", "✖"),
+#       Experimental = cell_spec(Experimental, color = ifelse(Experimental == "✔", "darkgreen", "red")),
+#       Labstudy = ifelse(Labstudy, "🏠", "🌍"),
+#       Study = text_spec(Study, link = links[Study])  # Match by study name
+#       #Study = text_spec(Study, link = links)
+#     )
+#   
+#   kbl(ft, escape = FALSE,
+#       col.names = c("Study",
+#                     "Effect expected",
+#                     "N",
+#                     "Trials",
+#                     "M",
+#                     "SD",
+#                     "Hits (%)",
+#                     "t",
+#                     "p",
+#                     "ES",
+#                     "Var",
+#                     "BF",
+#                     "Direction",
+#                     "Year",
+#                     "Lab/Online"),
+#       align = "lcccccccccccccc") %>%
+#     kable_classic("hover") %>%
+#     kable_styling(
+#       fixed_thead = TRUE,
+#       html_font = "Arial",
+#       bootstrap_options = c("striped", "hover", "condensed")
+#     )
+# }
+# 
+# 
+# generate_table <- function(experiment_name) {
+#   load("../data/study_objects.RData", envir = .GlobalEnv)
+#   st <- readRDS("../data/overview2025-07-12.rds")
+#   
+#   ft <- st %>%
+#     filter(grepl(experiment_name, Study)) %>%
+#     mutate(
+#       across(where(is.numeric), ~ round(., 2)),
+#       `Hits (%)` = formattable::color_tile("coral2", "cornflowerblue")(`Hits (%)`),
+#       p = cell_spec(p, bold = ifelse(p < 0.05, TRUE, FALSE)),
+#       Experimental = ifelse(Experimental, "✔", "✖"),
+#       Experimental = cell_spec(Experimental, color = ifelse(Experimental == "✔", "darkgreen", "red")),
+#       Labstudy = ifelse(Labstudy, "🏠", "🌍")
+#       # No Study = text_spec() here
+#     )
+#   
+#   kbl(ft, escape = FALSE,
+#       col.names = c("Study",
+#                     "Effect expected",
+#                     "N",
+#                     "Trials",
+#                     "M",
+#                     "SD",
+#                     "Hits (%)",
+#                     "t",
+#                     "p",
+#                     "ES",
+#                     "Var",
+#                     "BF",
+#                     "Direction",
+#                     "Year",
+#                     "Lab/Online"),
+#       align = "lcccccccccccccc") %>%
+#     kable_classic("hover") %>%
+#     kable_styling(
+#       fixed_thead = TRUE,
+#       html_font = "Arial",
+#       bootstrap_options = c("striped", "hover", "condensed")
+#     )
+# }
+
 
 
 generate_demographics_table <- function(data, age_col, gender_col, male_value, female_value, other_value = NULL) {
@@ -85,5 +237,7 @@ generate_demographics_table <- function(data, age_col, gender_col, male_value, f
   
   # Print the table using kable
   kable(table_data, format = "html", col.names = c("Characteristic", "Count/Statistic")) %>%
-    kable_styling(full_width = FALSE)
+    kable_styling(
+      full_width = FALSE
+      )
 }
